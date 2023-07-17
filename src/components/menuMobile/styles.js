eaffd92ff4b040b71d63107435bceca109438675
Wendell47 <ps4.wendell@gmail.com>
2023-07-17 19:15:08 -0400

    menu Mobile fix

--- src/components/menuMobile/styles.js
+++ src/components/menuMobile/styles.js
@@ -2,10 +2,11 @@ import styled from "styled-components";
 
 export const Container = styled.div`
 
-position: absolute;
+position: fixed;
 left: 0;
 right: 0;
 top: 0;
+bottom: 0;
 z-index: 3;
 width: 100%;
 height: 100%;
