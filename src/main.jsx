8fe55eb21e3a1d73fcece5dcb2d5a1a004f4e2c4
Wendell47 <ps4.wendell@gmail.com>
2023-08-04 18:24:55 -0400

    add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/index.css'
-
+import { inject } from '@vercel/analytics';
+ 
 import GlobalStyles from './styles/global'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import Routes from './routes'
 
 import {AuthProvider} from './hooks/auth'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -19,3 +22,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </ThemeProvider>
   </React.StrictMode>
 )
+inject();
