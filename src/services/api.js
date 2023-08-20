f29054ffe52deccb50974e721c6db7acf1431bf5
Wendell47 <ps4.wendell@gmail.com>
2023-08-20 16:04:08 -0400

    resolve api andress

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,6 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL:  "http://localhost:3333"
+    baseURL: "https://food-explorer-api-n53a.onrender.com" || "http://localhost:3333"
 })
 
